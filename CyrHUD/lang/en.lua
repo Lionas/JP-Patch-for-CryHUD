d51110a1781ca94b4437f68071d1707641bb916a
Lionas <lionas.watanabe@gmail.com>
2016-07-07 12:11:28 +0900

    多言語化対応

---
+++ CyrHUD/lang/en.lua
@@ -0,0 +1,4 @@
+CyrHUD:LoadLanguage {
+	name = "en",
+	font = "EsoUI/Common/Fonts/univers67.otf|18|soft-shadow-thick",
+}
---
+++ CyrHUD/lang/jp.lua
@@ -0,0 +1,4 @@
+CyrHUD:LoadLanguage {
+	name = "jp",
+	font = "$(CHAT_FONT)|18|soft-shadow-thick",
+}
